fef81d8694e20399e9d91ea154480a3a9d813ae3
noopy <young_ahn@yonsei.ac.kr>
2020-06-12 08:11:24 +0900

    crawling terms and links

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Investopedia Crawler
 
-Scrapes terms & definitions from investopedia
+Scrapes terms & definitions from [investopedia.com](https://www.investopedia.com/terms-beginning-with-a-4769351)
+Terms beginning with A ~ Zs are crawled and collected as csv files.
--- terms_crawler.py
+++ terms_crawler.py
@@ -5,12 +5,27 @@ import lxml
 import pandas as pd
 import string
 
-alphabet_list = string.ascii_lowercase
 
-for alphabet in alphabet_list:
-    index_no_int = 4769351 + alphabet_list.index(alphabet)
-    index_no_str = str(index_no_int)
-    terms_url = (
-        f"https://www.investopedia.com/terms-beginning-with-{alphabet}-{index_no_str}"
-    )
-    print(terms_url)
+def terms_listup(scrape_url):
+    # get page content response from the web using requests and beautifulsoup
+    res = requests.get(scrape_url)
+    soup = BeautifulSoup(res.content, "lxml")
+    # print(soup)
+    href_data = soup.find_all("a", href=True)
+    print(href_data)
+
+    return
+
+
+def all_terms_listup():
+    alphabet_list = string.ascii_lowercase
+
+    for alphabet in alphabet_list:
+        index_no_int = 4769351 + alphabet_list.index(alphabet)
+        index_no_str = str(index_no_int)
+        terms_url = f"https://www.investopedia.com/terms-beginning-with-{alphabet}-{index_no_str}"
+        print(terms_url)
+    return
+
+
+terms_listup("https://www.investopedia.com/terms-beginning-with-a-4769351")
