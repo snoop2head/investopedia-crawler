3b81827e3a859458f8e8d61b4a7acaac7f9b0ceb
noopy <young_ahn@yonsei.ac.kr>
2020-06-12 18:17:08 +0900

    collect summaries from individual vocabulary page

--- terms_crawler.py
+++ terms_crawler.py
@@ -7,10 +7,20 @@ import string
 
 
 def collect_three_lines(vocab_url):
-    return
+    # get page content response from the web using requests and beautifulsoup
+    res = requests.get(vocab_url)
+    soup = BeautifulSoup(res.content, "lxml")
+    # print(soup)
+    three_summary_div = soup.find(
+        "div", {"class": "comp mntl-sc-block-callout-body mntl-text-block"}
+    )
+    # print(three_summary_div)
+    three_line_summary = three_summary_div.get_text()
+    return three_line_summary
 
 
 def listup_terms(scrape_url):
+    VOCAB_BASE_URL = "https://www.investopedia.com/terms/"
     empty_list = []
     alphabet = scrape_url.split("-")[-2]
     print(alphabet)
@@ -21,8 +31,8 @@ def listup_terms(scrape_url):
     for item in soup.find_all("a", href=True):
         href_item = item["href"]
         # print(href_item)
-        term_url = "https://www.investopedia.com/terms/" + alphabet
-        if term_url in href_item:
+        vocab_url = VOCAB_BASE_URL + alphabet
+        if vocab_url in href_item:
             # print(href_item)
             empty_list.append(href_item)
             vocab_url_list = empty_list
@@ -31,13 +41,18 @@ def listup_terms(scrape_url):
 
 
 def listup_all_terms():
+    empty_list = []
+    INDEX_NO = 4769351
     alphabet_list = string.ascii_lowercase
     for alphabet in alphabet_list:
-        index_no_int = 4769351 + alphabet_list.index(alphabet)
+        index_no_int = INDEX_NO + alphabet_list.index(alphabet)
         index_no_str = str(index_no_int)
         terms_url = f"https://www.investopedia.com/terms-beginning-with-{alphabet}-{index_no_str}"
         print(terms_url)
-    return
+        empty_list.append(terms_url)
+        alphabet_url_list = empty_list
+    return alphabet_url_list
 
 
-# terms_listup("https://www.investopedia.com/terms-beginning-with-b-4769352")
+# listup_terms("https://www.investopedia.com/terms-beginning-with-b-4769352")
+collect_three_lines("https://www.investopedia.com/terms/b/buyersmarket.asp")
