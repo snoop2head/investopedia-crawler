2cbf953be4fc66c010b5a4adb03f479e2a547ed8
noopy <young_ahn@yonsei.ac.kr>
2020-06-13 14:04:52 +0900

    access mongodb atlas using pymongo

--- acess_db.py
+++ acess_db.py
@@ -1,1 +1,20 @@
-from pymongo
+import os
+from pymongo import MongoClient
+import pymongo
+
+MONGO_URL = os.getenv("MONGO_URL")
+print("Connecting MongoDB Atlas to: " + MONGO_URL)
+
+client = pymongo.MongoClient(MONGO_URL)
+
+"""
+# testing database connection
+db = client.test
+print(db)
+
+# testing collection connection
+db = client.get_database("vocab")
+retrieved_terms = db.vocab_terms
+all_documents_no = retrieved_terms.count_documents({})
+print(all_documents_no)
+"""
